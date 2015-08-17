089de85f3854a7744134d2e0f1cff0d710afa316
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-08-17 09:51:49 +0900

    refactor rename directories

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var path = require('path');
 var port = process.argv[2] || process.env.PORT || 3000;
 console.log('port:', port);
 var express = require('express'), app = express();
-app.use('/js',     express.static('js'));
-app.use('/css',    express.static('css'));
-app.use('/public', express.static('public'));
+['js', 'css', 'mui', 'mdl', 'mithril', 'public'].forEach(
+  function (dir) {
+    app.use('/' + dir, express.static(dir));
+  });
 app.get('/*', function (req, res) {
   if (req.url === '/' || req.url === '/index.html')
     res.sendFile(path.resolve('index.html'));
